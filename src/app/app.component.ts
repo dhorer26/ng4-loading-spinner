83bad38b39de62466c6d629bf00ae9497d4cc4c4
Ravindra Dhore <ravindrad@ansh.com>
2019-11-26 12:00:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'firebase-auth-angular-app';
+  constructor(private spinnerService: Ng4LoadingSpinnerService) {}
+  show() {
+    this.spinnerService.show();
+    setTimeout(() => this.spinnerService.hide(), 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng4LoadingSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
